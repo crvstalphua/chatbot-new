696ecca9e2266b3aa7e9e95a664b9fdc49bf0930
crvstalphua <>
2023-06-19 16:09:34 +0800

    modified code to take in keys as secrets

--- app/constants.py
+++ app/constants.py
@@ -11,11 +11,15 @@ import streamlit as st
 load_dotenv(find_dotenv())
 
 DOCUMENT = "Combined_MDW_OCR_16Jun23.pdf"
-INDEX_NAME = os.getenv('INDEX_NAME')
+INDEX_NAME = st.secret['INDEX_NAME']
+#INDEX_NAME = os.getenv('INDEX_NAME')
 MODEL_NAME = 'gpt-3.5-turbo'
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-PINECONE_API_ENV = os.getenv('PINECONE_API_ENV')
-PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
+OPENAI_API_KEY = st.secret['OPENAI_API_KEY']
+PINECONE_API_ENV = st.secret['PINECONE_API_ENV']
+PINECONE_API_KEY = st.secret['PINECONE_API_KEY']
+#OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+#PINECONE_API_ENV = os.getenv('PINECONE_API_ENV')
+#PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
 TEMPERATURE = 0
 
 # INDEX_NAME == st.secrets["INDEX_NAME"]
